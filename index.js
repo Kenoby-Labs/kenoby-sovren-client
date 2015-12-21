f64669efb0da3ba855dd9bf5cad97a80b80aa02d
Jonathan Borges <jonathanbsilva@gmail.com>
2015-12-21 18:13:12 -0200

    Add xml2json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var soap, async, parser;
 soap = require('soap');
 async = require('async');
-parser = require('./lib/xml2json');
+parser = require('xml2json');
 
 module.exports = function sovrenClient(url, id, key) {
   return function (file, next) {
