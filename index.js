51a80615f21e5d449c2d941632671f8734b45f69
Jonathan Borges <jonathanbsilva@gmail.com>
2015-12-23 09:33:10 -0200

    Add promise

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 'use strict';
 
-var soap, async, parser;
+var q, soap, async, parser;
+q = require('q');
 soap = require('soap');
 async = require('async');
 parser = require('xml2json');
 
 module.exports = function sovrenClient(url, id, key) {
   return function (file, next) {
-    async.waterfall([function (next) {
-      soap.createClient(url, next);
-    }, function (client, next) {
-      client.ParsingService.ParsingServiceSoap12.ParseResume({'request' : {
-        'AccountId'     : id,
-        'ServiceKey'    : key,
-        'FileBytes'     : (new Buffer(file)).toString('base64'),
-        'Configuration' : '_100000_0_00000001_1101010110001101_1_0001111111111111111102011001101110000110000000000010000000100'
-      }}, next);
-    }, function (data, _, a, next) {
-      next(null, parser.toJson(data.ParseResumeResult.Xml, {'object': true}));
-    }], next);
+    var deferred = q.defer();
+
+    async.waterfall([
+      function (next) {
+        soap.createClient(url, next);
+      },
+      function (client, next) {
+        client.ParsingService.ParsingServiceSoap12.ParseResume({'request' : {
+          'AccountId'     : id,
+          'ServiceKey'    : key,
+          'FileBytes'     : (new Buffer(file)).toString('base64'),
+          'Configuration' : '_100000_0_00000001_1101010110001101_1_0001111111111111111102011001101110000110000000000010000000100'
+        }}, next);
+      },
+      function (data, _, a, next) {
+        next(null, parser.toJson(data.ParseResumeResult.Xml, {'object': true}));
+      }
+    ], function (error, parsed) {
+      if (error) {
+        deferred.reject(new Error(error));
+      } else {
+        deferred.resolve(parsed);
+      }
+    });
+
+    return deferred.promise;
   };
 };
